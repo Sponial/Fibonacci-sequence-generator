b9d3b67ce78b111a4677bcbf63fce0798b705ecf
Orson Stawiarski <orson@stazca.com>
2026-02-25 15:39:52 +0700

    implement while statement, already can see flaws in it but just getting the logic in the right place first and seeing where it breaks from there

---
+++ fibonnaciGenerator.js
@@ -0,0 +1,14 @@
+var output = [0]
+
+function fibonnaciGenerator (n) {
+    for (var output = 0, output < 10, ) //This is just an example that I borrowed. 
+    var position1 = output + 1;
+    output = position1
+    output.push(position1)
+    n = output
+    return n
+}
+
+
+//Okay, so we need to have an array built that will create items that are always the product of multiplying the previous two arrays together. You have a variable that takes you have a variable that pushes an output that is always the last two positions added together. And then after which, each time, the positions that it takes is incremented up. 
+//Second floor using a 4 control statement might be the correct way to go about this. If we can control the rate of growth, end number would have to be somehow relating to the return, and then the starting number would just be a simple 1, then we should be able to have a consistent rate of growth. The hard part would just be getting it to be that's parameters set based on the return that is input. 
