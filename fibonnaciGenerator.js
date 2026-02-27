d1f9629309a974d32da75704e3bdc6957a2fad1f
Orson Stawiarski <orson@stazca.com>
2026-02-27 23:32:17 +0700

    add console.log before the function call, succcessfully produced an output from node, this was just misunderstanding node

--- fibonnaciGenerator.js
+++ fibonnaciGenerator.js
@@ -5,11 +5,10 @@ for (output = [0]; output.length < n; output.push(output[output.length - 1] + ou
 else if (n === 2) {output.push(1,1)}
 }
 
-return output[n];
-console.log(output[n]);
+return output;
     }
 
-fibonnaciGenerator(5);
+console.log(fibonnaciGenerator(5));
 //Okay, so we need to have an array built that will create items that are always the product of multiplying the previous two arrays together. You have a variable that takes you have a variable that pushes an output that is always the last two positions added together. And then after which, each time, the positions that it takes is incremented up. 
 //Second floor using a 4 control statement might be the correct way to go about this. If we can control the rate of growth, end number would have to be somehow relating to the return, and then the starting number would just be a simple 1, then we should be able to have a consistent rate of growth. The hard part would just be getting it to be that's parameters set based on the return that is input. 
 //We need to be able to convert the length of the array into a position in which whatever is input on N can then be assigned to a position rather than a raw. 
