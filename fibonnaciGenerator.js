c8b3cfae586aa4aeec5d83a36d4ed36811074cf7
Orson Stawiarski <orson@stazca.com>
2026-02-27 15:02:56 +0700

    output[n] returned undefined

--- fibonnaciGenerator.js
+++ fibonnaciGenerator.js
@@ -1,9 +1,9 @@
 function fibonnaciGenerator(n) {
-for (var output = [0]; output.length < n; output[output.length - 1] + output[output.length -2] {
+for (var output = [0]; output.length < n; output[output.length - 1] + output[output.length -2]) {
     if (n === 1) {output.push(0)}
 else if (n === 2) {output.push(0,1)}
 }
-return n
+return output[n];
     }
 
 //Okay, so we need to have an array built that will create items that are always the product of multiplying the previous two arrays together. You have a variable that takes you have a variable that pushes an output that is always the last two positions added together. And then after which, each time, the positions that it takes is incremented up. 
